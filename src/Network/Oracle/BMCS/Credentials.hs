bd45cf76463b10be8486ee8fb0fadc6bafc860f4
Owain Lewis <owain@owainlewis.com>
2017-01-29 20:53:32 +0000

    Updates credentials parse examples

--- src/Network/Oracle/BMCS/Credentials.hs
+++ src/Network/Oracle/BMCS/Credentials.hs
@@ -21,11 +21,12 @@ import Data.Ini
 defaultLocation :: IO FilePath
 defaultLocation = (\homeDir -> homeDir <> "/.oraclebmc/config") <$> getEnv "HOME"
 
--- [DEFAULT]
--- user=ocid1.user.oc1..aaaaaaaat5nvwcna5j6aqzjcmdy5eqbb6qt2jvpkanghtgdaqedqw3rynjq
--- fingerprint=20:3b:97:13:55:1c:5b:0d:d3:37:d8:50:4e:c5:3a:34
--- key_file=~/.oraclebmc/bmcs_api_key.pem
--- tenancy=ocid1.tenancy.oc1..aaaaaaaaba3pv6wkcr4jqae5f15p2bcmdyt2j6rx32uzr4h25vqstifsfdsq
+-- Example credentials
+--   [DEFAULT]
+--   user=ocid1.user.oc1..aaaaaaaat5nvwcna5j6aqzjcmdy5eqbb6qt2jvpkanghtgdaqedqw3rynja
+--   fingerprint=20:3b:97:13:55:1c:5b:0d:d3:37:d8:50:4e:c5:3a:39
+--   key_file=~/.oraclebmc/bmcs_api_key.pem
+--   tenancy=ocid1.tenancy.oc1..aaaaaaaaba3pv6wkcr4jqae5f15p2bcmdyt2j6rx32uzr6h25vqstifsfdsq
 --
 data Credentials = Credentials {
     user :: T.Text
@@ -34,10 +35,15 @@ data Credentials = Credentials {
   , tenancy :: T.Text
 } deriving ( Eq, Show )
 
+parseCredentials :: Ini -> Either String Credentials
 parseCredentials ini = do
-    lookupValue "DEFAULT" "user" ini
-    return "OK"
+    user <- lookupValue "DEFAULT" "user" ini
+    fingerprint <- lookupValue "DEFAULT" "fingerprint" ini
+    keyFile <- lookupValue "DEFAULT" "key_file" ini
+    tenancy <- lookupValue "DEFAULT" "tenancy" ini
+    return $ Credentials user fingerprint keyFile tenancy
 
+fromFile :: FilePath -> IO (Either String Credentials)
 fromFile path = do
     contents <- TIO.readFile path
     return $ (parseIni contents) >>= parseCredentials
