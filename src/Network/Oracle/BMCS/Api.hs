69daeb76eccc5e39df8c75947e659203260c65c5
Owain Lewis <owain@owainlewis.com>
2017-01-29 16:29:41 +0000

    Parsing config

---
+++ src/Network/Oracle/BMCS/Api.hs
@@ -0,0 +1,1 @@
+module Network.Oracle.BMCS.Api where
--- src/Network/Oracle/BMCS/Credentials.hs
+++ src/Network/Oracle/BMCS/Credentials.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Network.Oracle.BMCS.Credentials
@@ -11,6 +12,15 @@ module Network.Oracle.BMCS.Credentials
     ( Credentials(..)
     ) where
 
+import Data.Text as T
+import Data.Text.IO as TIO
+import System.Environment(getEnv)
+import Data.Semigroup((<>))
+import Data.Ini
+
+defaultLocation :: IO FilePath
+defaultLocation = (\homeDir -> homeDir <> "/.oraclebmc/config") <$> getEnv "HOME"
+
 -- [DEFAULT]
 -- user=ocid1.user.oc1..aaaaaaaat5nvwcna5j6aqzjcmdy5eqbb6qt2jvpkanghtgdaqedqw3rynjq
 -- fingerprint=20:3b:97:13:55:1c:5b:0d:d3:37:d8:50:4e:c5:3a:34
@@ -18,8 +28,16 @@ module Network.Oracle.BMCS.Credentials
 -- tenancy=ocid1.tenancy.oc1..aaaaaaaaba3pv6wkcr4jqae5f15p2bcmdyt2j6rx32uzr4h25vqstifsfdsq
 --
 data Credentials = Credentials {
-    user :: String
-  , fingerprint :: String
-  , keyFile :: String
-  , tenancy :: String
+    user :: T.Text
+  , fingerprint :: T.Text
+  , keyFile :: T.Text
+  , tenancy :: T.Text
 } deriving ( Eq, Show )
+
+parseCredentials ini = do
+    lookupValue "DEFAULT" "user" ini
+    return "OK"
+
+fromFile path = do
+    contents <- TIO.readFile path
+    return $ (parseIni contents) >>= parseCredentials
