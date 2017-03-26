a3c9c0308c76e381da8c146c84849d1a5ac65993
Owain Lewis <owain@owainlewis.com>
2017-03-26 18:45:26 +0100

    shouldn't be this hard

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ Haskell SDK for Oracle Bare Metal Cloud Services.
 
 This library provides a Haskell interface for working with the Oracle Bare Metal Cloud
 
+
+Required
+
+```
+sudo apt-get install libssl-dev
+```
+
 ## Credentials
--- src/Network/Oracle/BMC/Signature.hs
+++ src/Network/Oracle/BMC/Signature.hs
@@ -1,40 +1,88 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Oracle.BMC.Signature where
 
--- Used to sign HTTP request to the Oracle BMCS API
-
 import qualified Codec.Crypto.RSA        as RSA
-
-import           Codec.Crypto.RSA        (sign)
-import           Crypto.PubKey.OpenSsh   (OpenSshPrivateKey (..), decodePrivate)
-import           Crypto.Types.PubKey.RSA (PrivateKey)
-
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Lazy    as LBS
+import qualified Data.ByteString.Lazy.Char8 as C
+import           Crypto.Types.PubKey.RSA (PrivateKey, PublicKey)
 
-throwLeftIO :: (Monad m, Show a) => m (Either a b) -> m b
-throwLeftIO either = do
-  x <- either
-  case x of
-    Left e  -> error (show e)
-    Right x -> return x
+import OpenSSL.PEM
+import OpenSSL.EVP.PKey
+import OpenSSL.RSA
+import Data.Maybe
 
-loadPrivateKey :: FilePath -> IO (Either String PrivateKey)
-loadPrivateKey keyPath = do
-    contents <- BS.readFile keyPath
-    case (decodePrivate contents) of
-      Right (OpenSshPrivateKeyRsa k) -> return $ Right k
-      Right _                        -> return $ Left "Invalid key type"
-      Left e                         -> return $ Left e
+defaultPath = "/home/owainlewis/.oraclebmc/bmcs_api_key.pem"
 
-loadSSHPrivateKey :: FilePath -> IO PrivateKey
-loadSSHPrivateKey = throwLeftIO . loadPrivateKey
+loadKey :: IO RSAKeyPair
+loadKey = do
+      keyString <- readFile defaultPath
+      key <- readPrivateKey keyString PwNone
+      let maybeRsaKey = (toKeyPair key) :: Maybe RSAKeyPair
+      return $ fromJust maybeRsaKey
 
--- Sign and input with some private key
---
-signWithKey :: FilePath -> LBS.ByteString -> IO (LBS.ByteString)
-signWithKey keyPath input = flip RSA.sign input <$> loadSSHPrivateKey keyPath
+publicKey kp =
+  let size = 2048
+      n = rsaN (kp)
+      e = rsaE (kp)
+      d = rsaD (kp)
+      p = rsaP (kp)
+      q = rsaQ (kp)
+      dp = d `mod` (p-1)
+      dq = d `mod` (q-1)
+      qinv = (q^(-1)) `mod` p
+      publicKeyRSA = RSA.PublicKey size n e
+  in
+  RSA.PrivateKey publicKeyRSA d p q dp dq qinv
 
--- | Bytestrings probably easier here
---
-signWithKey' :: FilePath -> BS.ByteString -> IO BS.ByteString
-signWithKey' keyPath input = LBS.toStrict <$> signWithKey keyPath (LBS.fromStrict input)
+signInput input = (\k -> RSA.sign (publicKey k) input) <$> loadKey
+
+-- -- Sign and input with some private key
+-- --
+--signWithKey :: FilePath -> LBS.ByteString -> IO (LBS.ByteString)
+--signWithKey keyPath input = flip RSA.sign input <$> loadKey
+
+-- -- Used to sign HTTP request to the Oracle BMCS API
+
+-- import qualified Codec.Crypto.RSA        as RSA
+
+-- import           Codec.Crypto.RSA        (sign)
+-- import           Crypto.PubKey.OpenSsh   (OpenSshPrivateKey (..), decodePrivate)
+-- import           Crypto.Types.PubKey.RSA (PrivateKey)
+
+-- import qualified Data.ByteString         as BS
+-- import qualified Data.ByteString.Lazy    as LBS
+
+-- import qualified Data.PEM as Pem
+
+-- import qualified OpenSSL.PEM as P
+-- import qualified OpenSSL.RSA as R
+
+-- throwLeftIO :: (Monad m, Show a) => m (Either a b) -> m b
+-- throwLeftIO either = do
+--   x <- either
+--   case x of
+--     Left e  -> error (show e)
+--     Right x -> return x
+
+-- loadPrivateKey :: FilePath -> IO (Either String PrivateKey)
+-- loadPrivateKey keyPath = do
+--     contents <- BS.readFile keyPath
+--     case (decodePrivate contents) of
+--       Right (OpenSshPrivateKeyRsa k) -> return $ Right k
+--       Right x                        -> return $ Left $ "E"
+--       Left e                         -> return $ Left "Fail"
+
+-- loadSSHPrivateKey :: FilePath -> IO PrivateKey
+-- loadSSHPrivateKey = throwLeftIO . loadPrivateKey
+
+-- -- Sign and input with some private key
+-- --
+-- signWithKey :: FilePath -> LBS.ByteString -> IO (LBS.ByteString)
+-- signWithKey keyPath input = flip RSA.sign input <$> loadSSHPrivateKey keyPath
+
+-- defaultPath = "/home/owainlewis/.oraclebmc/bmcs_api_key.pem"
+
+
+
+
