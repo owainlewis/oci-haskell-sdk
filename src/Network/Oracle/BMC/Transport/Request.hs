388e2dfbd489aae24825f0b07f2ca9da89105210
Owain Lewis <owain@owainlewis.com>
2017-03-29 08:21:07 +0100

    Minor updates to get a failing example request working

--- src/Network/Oracle/BMC/Transport/Request.hs
+++ src/Network/Oracle/BMC/Transport/Request.hs
@@ -1,41 +1,56 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Oracle.BMC.Transport.Request where
 
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
-import Network.HTTP.Types.Status (statusCode)
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
+import           Network.HTTP.Types.Status (statusCode)
 
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString           as BS
+import qualified Data.ByteString.Char8     as C8
+import qualified Data.ByteString.Lazy      as LBS
+import qualified Data.CaseInsensitive      as CI
 
-import qualified Data.ByteString.Lazy as LBS
+-- | Types
+-----------------------------------------------------------------------------------------
 
-import qualified Data.CaseInsensitive as CI
+data HttpMethod = GET
+                | POST
+                | PUT
+                | DELETE
+                | PATCH
+                deriving ( Eq, Read, Show )
 
-base = "https://iaas.us-phoenix-1.oraclecloud.com"
+data HttpRequest = HttpRequest { httpMethod :: HttpMethod
+                               , url :: String
+                               , headers :: [(BS.ByteString, BS.ByteString)]
+                               , body :: Maybe BS.ByteString
+                               } deriving ( Eq, Read, Show )
 
--- A simple abstraction for HTTP requests to make it easier to test and verify HTTP inputs
-data OracleRequest = OracleRequest { url :: String
-                                   , requestMethod :: String
-                                   , headers :: [(BS.ByteString, BS.ByteString)]
-                                   , body :: Maybe BS.ByteString
-                                   } deriving ( Eq, Ord, Read, Show )
+-- | Request / Response
+-----------------------------------------------------------------------------------------
 
-requiredHeaders = ["date", "(request-target)", "host"]
-
-transformRequest :: OracleRequest -> IO Network.HTTP.Client.Request
+transformRequest :: HttpRequest -> IO Network.HTTP.Client.Request
 transformRequest request = do
       manager <- newManager tlsManagerSettings
       initialRequest <- parseRequest (url request)
       let req = initialRequest
-                    { method = C8.pack (requestMethod request)
+                    { method = C8.pack (show . httpMethod $ request)
                     , requestHeaders = map (\(k, v) -> (CI.mk k, v)) (headers request)
                     }
       return $ maybe req (\lbs -> req { requestBody = RequestBodyBS lbs }) (body request)
 
-runHttpsRequest :: OracleRequest -> IO (Network.HTTP.Client.Response LBS.ByteString)
+-- | TODO pass options into here
+runHttpsRequest :: HttpRequest -> IO (Network.HTTP.Client.Response LBS.ByteString)
 runHttpsRequest req = do
     manager <- newManager tlsManagerSettings
     internalRequest <- transformRequest req
     response <- httpLbs internalRequest manager
     return response
+
+-- | Domain specific
+-----------------------------------------------------------------------------------------
+
+requiredHeaders = ["date", "(request-target)", "host"]
+
+example = runHttpsRequest (HttpRequest GET url [] Nothing)
+    where url = "https://iaas.us-phoenix-1.oraclecloud.com/20160918/instances?availabilityDomain=Pjwf%3A%20PHX-AD-1&compartmentId=ocid1.compartment.oc1..aaaaaaaam3we6vgnherjq5q2idnccdflvjsnog7mlr6rtdb25gilchfeyjxa&displayName=TeamXInstances&volumeId=ocid1.volume.oc1.phx.abyhqljrgvttnlx73nmrwfaux7kcvzfs3s66izvxf2h4lgvyndsdsnoiwr5q"
