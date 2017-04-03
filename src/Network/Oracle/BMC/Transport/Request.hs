637375fbdef9659e82638e806192f6d8797fd2a1
Owain Lewis <owain@owainlewis.com>
2017-04-03 19:07:11 +0100

    Train updates

--- src/Network/Oracle/BMC/Transport/Request.hs
+++ src/Network/Oracle/BMC/Transport/Request.hs
@@ -6,6 +6,8 @@ module Network.Oracle.BMC.Transport.Request
     , URL
     , Header
     , runHttpsRequest
+    , putHeader
+    , putHeaderIfAbsent
     , get
     , post
     , put
@@ -20,6 +22,8 @@ import qualified Data.ByteString           as BS
 import qualified Data.ByteString.Char8     as C8
 import qualified Data.ByteString.Lazy      as LBS
 import qualified Data.CaseInsensitive      as CI
+
+import Data.Set as Set hiding ( delete )
 
 -- | Types
 -------------------------------------------------------------------------------
@@ -43,23 +47,24 @@ data HttpRequest = HttpRequest { httpMethod :: HttpMethod
                                , query :: Maybe [(BS.ByteString, BS.ByteString)]
                                } deriving ( Eq, Read, Show )
 
-withHeader :: HttpRequest -> Header -> HttpRequest
-withHeader request@(HttpRequest _ _ h _ _) header =
-    request { headers = header : h }
-
 type HttpResponse = Network.HTTP.Client.Response LBS.ByteString
+
+
+-- | Request contsruction
+-------------------------------------------------------------------------------
+
 -- | Construct a HTTP GET request
 --
---   You can also set the query string
+--   You can also set the query string using a list of (k,v) tuples
 --
--- > λ> (get "https://github.com" []) { query = Just [] }
+-- > λ> (get "https://github.com" []) { query = Just [("foo", "bar")] }
 --
 -- > HttpRequest {
 --     httpMethod = GET
 --   , url = "https://github.com"
 --   , headers = []
 --   , body = Nothing
---   , query = Just []
+--   , query = Noting
 --   }
 --
 get :: URL -> [Header] -> HttpRequest
@@ -74,6 +79,25 @@ put url headers body = HttpRequest PUT url headers body Nothing
 delete :: URL -> [Header] -> HttpRequest
 delete url headers = HttpRequest DELETE url headers Nothing Nothing
 
+-------------------------------------------------------------------------------
+
+
+-- | TODO This isn't a great datastructure to be using. Given there aren't many
+--   http headers the performance is less of an issue but users having to import
+--   containers as a library was the main concern
+--
+putHeader :: HttpRequest -> Header -> HttpRequest
+putHeader request header =
+    request { headers = header : (headers request) }
+
+putHeaderIfAbsent :: HttpRequest -> Header -> HttpRequest
+putHeaderIfAbsent request header =
+    if headerExists (headers request) header then putHeader request header
+                                             else request
+      where headerExists [] h = False
+            headerExists (x:xs) h =
+                if x == h then True else headerExists xs h
+
 -- | Request / Response
 -------------------------------------------------------------------------------
 
@@ -85,7 +109,7 @@ transformRequest request =
             initialRequest <- parseRequest (url request)
             let initReq = initialRequest
                               { method = C8.pack (show . httpMethod $ request)
-                              , requestHeaders = map (\(k, v) -> (CI.mk k, v)) (headers request)
+                              , requestHeaders = Prelude.map (\(k, v) -> (CI.mk k, v)) (headers request)
                               }
                 req = maybe initReq (\qs -> setQueryString (secondJust <$> qs) initReq) (query request)
             return $ maybe req (\lbs -> req { requestBody = RequestBodyBS lbs }) (body request)
--- src/Network/Oracle/BMC/Transport/Specialized.hs
+++ src/Network/Oracle/BMC/Transport/Specialized.hs
@@ -2,29 +2,64 @@
 module Network.Oracle.BMC.Transport.Specialized where
 
 import qualified Network.HTTP.Client                  as Client
+import           Network.Oracle.BMC.Transport.Request (HttpRequest)
 import qualified Network.Oracle.BMC.Transport.Request as Request
-import qualified Network.URI as URI
+import qualified Network.URI                          as URI
 
-import Data.Maybe(fromMaybe)
+import           Data.Maybe                           (fromMaybe)
 
 import qualified Data.ByteString                      as BS
+import qualified Data.ByteString.Char8                as C8
 
-import qualified Data.Monoid as Monoid
+import qualified Data.Monoid                          as Monoid
 
 import           Data.Time                            (defaultTimeLocale,
                                                        formatTime, getZonedTime)
 
 getNow :: IO String
-getNow = formatTime defaultTimeLocale "%a, %d %b %Y %H:%M:%S %Z" <$> getZonedTime
+getNow = formatTime defaultTimeLocale tf <$> getZonedTime
+    where tf = "%a, %d %b %Y %H:%M:%S %Z"
 
+-- Required on all get | delete requests
+--
 genericHeaders :: [BS.ByteString]
 genericHeaders = ["(request-target)", "host", "date"]
 
+-- Required on all post | put | patch requests
+--
 bodyHeaders :: [BS.ByteString]
 bodyHeaders = ["x-content-sha256", "content-type", "content-length"]
 
-getRequestPath :: Request.HttpRequest -> Maybe String
-getRequestPath request = URI.uriPath <$> URI.parseURI (Request.url request)
+getRequestPath :: HttpRequest -> String
+getRequestPath request = fromMaybe Monoid.mempty maybePath
+    where maybePath = (URI.uriPath <$> URI.parseURI (Request.url request))
 
---withRequestTarget request@(Request.HttpRequest m u h b q) =
---    fmap (\p -> show m ++ p ++ (fromMaybe "" q)) (getRequestPath request)
+-- Adds the date header to a request
+--
+withDate :: HttpRequest -> IO HttpRequest
+withDate request = do
+    now <- C8.pack <$> getNow
+    return $ Request.putHeaderIfAbsent request ("date", now)
+
+withRequestTarget :: HttpRequest -> HttpRequest
+withRequestTarget request =
+    let m = show (Request.httpMethod request)
+        p = getRequestPath request
+        q = ""
+        requestTarget = concat [m, " ", p, " ", q]
+    in Request.putHeaderIfAbsent request ("(request-target)", C8.pack requestTarget)
+
+withMaybeHost request =
+  let u = Request.url request in
+  do
+    uri <- URI.parseURI (Request.url request)
+    authority <- URI.uriAuthority uri
+    let host = URI.uriRegName authority
+    return $ Request.putHeaderIfAbsent request ("host", C8.pack host)
+
+withHost :: HttpRequest -> HttpRequest
+withHost request = fromMaybe request (withMaybeHost request)
+
+addMissingHeaders :: HttpRequest -> IO HttpRequest
+addMissingHeaders request = do
+    withDate request >>= pure . withRequestTarget
