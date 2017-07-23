d6a972d5aec67f1487c719f150d2ad41050ab5da
Owain Lewis <owain@owainlewis.com>
2017-07-23 18:09:08 +0100

    Update OCID.hs

--- src/Network/Oracle/BMC/Model/OCID.hs
+++ src/Network/Oracle/BMC/Model/OCID.hs
@@ -1,3 +1,8 @@
-module Network.Oracle.BMC.Model.OCID where
+module Network.Oracle.BMC.Model.OCID 
+  ( ocidPattern
+  ) where
 
 ocidPattern = "^([0-9a-zA-Z-_]+[.:])([0-9a-zA-Z-_]*[.:]){3,}([0-9a-zA-Z-_]+)$"
+
+isOCID :: String -> Bool
+isOCID = error "TODO"
