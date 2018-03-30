373a9bb458beb0086708fb7ee8c62410bbfa4dba
Owain Lewis <owain@owainlewis.com>
2018-03-30 14:48:14 +0100

    Flesh out the request structure

--- src/Network/Oracle/OCI/Common/Request.hs
+++ src/Network/Oracle/OCI/Common/Request.hs
@@ -1,14 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Oracle.OCI.Common.Request
-  ( Request(..)
-  , Method(..)
+  ( OCIRequest(..)
+  , toRequest
   ) where
 
-import           Data.ByteString as BS
+import           Data.ByteString      as BS
+import           Network.HTTP.Simple
 
-data Method = GET | PUT | POST | DELETE deriving ( Eq, Ord, Show )
+import qualified Data.ByteString.Lazy as LBS
+import qualified Network.HTTP.Client  as H
+import qualified Network.HTTP.Types   as H
 
-data Request = Request {
-    method  :: Method
-  , headers :: [(String, String)]
-  , body    :: BS.ByteString
-}
+type Pair = (BS.ByteString, BS.ByteString)
+
+data OCIRequest = OCIRequest {
+    host        :: BS.ByteString
+  , path        :: BS.ByteString
+  , method      :: BS.ByteString
+  , body        :: Maybe BS.ByteString
+  , headers     :: [Pair]
+  , queryString :: Maybe [Pair]
+} deriving ( Eq, Ord, Show )
+
+--addQueryString = -- $ setRequestQueryString [("compartmentId", Just "ocid1.tenancy.oc1..aaaaaaaaxf3fuazosc6xng7l75rj6uist5jb6ken64t3qltimxnkymddqbma")]
+
+toRequest :: OCIRequest -> H.Request
+toRequest (OCIRequest host path method hdrs body query) =
+    let basicRequest = setRequestHost host
+                     $ setRequestPath path
+                     $ setRequestMethod method
+                     $ setRequestSecure True
+                     $ H.defaultRequest in
+      basicRequest
